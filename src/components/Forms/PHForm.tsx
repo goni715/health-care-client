549fa51a319bfce9ab99572385fc8d5f4c3d8596
goni715 <halesgoni715149123@gmail.com>
2024-12-02 12:20:55 +0600

    zod resolver configure

--- src/components/Forms/PHForm.tsx
+++ src/components/Forms/PHForm.tsx
@@ -1,13 +1,31 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ReactNode } from "react";
 import { useForm, FormProvider, FieldValues, SubmitHandler } from "react-hook-form";
+import { ZodEffects, ZodObject } from "zod";
+
+type TFormConfig = {
+    resolver: any,
+    defaultValues?: Record<string, unknown>
+}
 
 type TProps = {
     children: ReactNode,
-    onSubmit: SubmitHandler<FieldValues>
+    onSubmit: SubmitHandler<FieldValues>,
+    schema: ZodObject<any> | ZodEffects<any>,
+    defaultValues?: Record<string, unknown>
 }
 
-const PHForm = ({children, onSubmit}: TProps) => {
-    const methods = useForm();
+const PHForm = ({children, onSubmit, schema, defaultValues}: TProps) => {
+    const formConfig : TFormConfig = {
+        resolver: zodResolver(schema)
+    }
+    
+    //set defaultValues
+    if(defaultValues){
+        formConfig['defaultValues'] = defaultValues;
+      }
+
+    const methods = useForm(formConfig);
     
     const submit : SubmitHandler<FieldValues> = async (data) => {
        await onSubmit(data);
