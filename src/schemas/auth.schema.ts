ad3ef2fe326628b263740e6b754ba523aa2bd410
goni715 <halesgoni715149123@gmail.com>
2024-12-02 12:27:41 +0600

    login schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,12 +1,24 @@
+import { NonWhiteSpaceRegex } from "@/constants/global.constant";
 import { z } from "zod";
 
+
 export const LoginSchema = z.object({
-  id: z
-    .string({ required_error: "ID is required" })
-    .min(1, { message: "ID is reuired" }),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .min(1, { message:  "Email is required" })
+    .trim()
+    .email({ message: "Invalid email address" }),
   password: z
-    .string({ required_error: "Password is required" })
-    .min(1, { message: "Password is required" })
-    .min(6, { message: "Password must be minimum 6 characters" })
-    .max(20, { message: "Password can't be more than 20 characters"})
+    .string({
+      required_error: "Password is required !",
+    })
+    .min(1, { message:  "Password is required !" })
+    .min(6, { message: "Password minimum 6 characters" })
+    .trim()
+    .max(60, { message: "Password maximum 60 characters" })
+    .refine((value) => NonWhiteSpaceRegex.test(value), {
+      message: "Password must not contain Whitespaces!",
+    }),
 });
