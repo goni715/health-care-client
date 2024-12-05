1f54e5455d41c462486582f1c9f47fede3166129
goni715 <halesgoni715149123@gmail.com>
2024-12-05 12:04:01 +0600

    customize modal

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import PHModal from "@/components/Modal/PHModal/PHModal";
+import { Box, Stack, Button, TextField } from "@mui/material";
 
 const SpecialtiesPage = () => {
-    return (
-        <>
-          <h1>Specialties Page</h1>   
-        </>
-    );
+
+  
+  return (
+    <>
+      <Box>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Button>Create Specialty</Button>
+          <PHModal/>
+          {/* <SpecialtyModal open={isModalOpen} setOpen={setIsModalOpen} /> */}
+          <TextField size="small" placeholder="Search Specialist" />
+        </Stack>
+      </Box>
+    </>
+  );
 };
 
 export default SpecialtiesPage;
